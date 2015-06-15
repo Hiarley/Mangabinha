32403ba7c1dc5e2850c063d794733f100d43f597
Hiarley Farias <hiarley.farias@gmail.com>
2015-06-15 11:48:41 -0300

    Função lista alunos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,45 @@
 #include <QLabel>
 
 
+
+QStandardItemModel *model2 = new QStandardItemModel();
+
+
+void MainWindow::criarListaPresenca(){
+    QStringList headers;
+    headers << tr("Matricula") << tr("Nome") << tr("Presença") << tr("Data");
+    model2->setHorizontalHeaderLabels(headers);
+    QList<QStandardItem *> listaItem;
+    // caminho e nome do arquivo
+    QFile arquivo("/home/hiarley/Magabinha/listaAlunos");
+    // variáveis que guardarão o conteúdo e a linha atual do arquivo
+    QString conteudo, linha;
+    // o arquivo foi aberto com sucesso?
+    if(arquivo.open(QFile::ReadOnly)){
+            // vamos criar uma nova instância da classe QTextStream
+            QTextStream entrada(&arquivo);
+            // vamos ler do arquivo uma linha de cada ver
+            //Matricula do aluno
+           QString matricula = entrada.readLine();
+                               listaItem << new QStandardItem("matricula");
+            // Nome do Aluno
+            QString nome = entrada.readLine();
+                    listaItem << new QStandardItem(nome);
+            //Numero do Data -- Pegar do Sistema
+            QString data = entrada.readLine();
+                    listaItem << new QStandardItem(data);
+            //Numero do Presença -- 1 falta/2 falta/Presente
+            QString presenca = entrada.readLine();
+                    listaItem << new QStandardItem(presenca);
+                               model2->appendRow(listaItem);
+    }else{
+
+    }
+
+
+           model2->appendRow(listaItem);
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -24,10 +63,11 @@ model->clear();
     label->setPixmap(pixmap);
     label->setVisible(true);
     listaItem<<label;
-
+    criarListaPresenca();
      // Previously declared as QStringListModel *strListM1
     //model->appendRow(listaItem);
     ui->listView->setModel(model);
+    ui->tableView->setModel(model2);
 
 
 }
